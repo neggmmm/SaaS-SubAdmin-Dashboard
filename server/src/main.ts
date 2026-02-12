3d4c4198e92cf03b6a2a0daf0eebc9df90b6494b
neggmmm <kekkdead@gmail.com>
2026-02-12 22:48:11 +0200

    CORS origin fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Your Vite dev server
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
